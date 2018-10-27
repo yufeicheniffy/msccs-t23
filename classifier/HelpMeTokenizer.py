3bb3979c60242974973367953deb132a26c8958e
Regan Dvoskin <rdvoskin@ccs.neu.edu>
2018-10-27 18:42:01 +0100

    remove stopwords and repeated words

--- classifier/HelpMeTokenizer.py
+++ classifier/HelpMeTokenizer.py
@@ -12,9 +12,20 @@ class HelpMeTokenizer(TweetTokenizer):
 	def process(self, text):
 		"""
 		Tokenize tweet, remove stopwords, 
-		twitter handles.
+		remove repeated words, twitter handles.
 		"""
 		tokenized = self.tokenize(text)
 		#print(tokenized)
-		
+		print(type(tokenized))
+
+		# remove stopwords
+		no_stopwords = [x for x in tokenized if not x in corp.stopwords.words()]
+
+		# remove repeated words
+		no_repeats = list(set(no_stopwords))
+
+
+		print(no_repeats)
+		return no_repeats
+
 
