2f9b68f484ffef43b0c71da865fbd1ca68794afd
Regan Dvoskin <rdvoskin@ccs.neu.edu>
2018-10-27 18:31:34 +0100

    basic working tokenizer

--- classifier/HelpMeTokenizer.py
+++ classifier/HelpMeTokenizer.py
@@ -4,6 +4,17 @@ from nltk.tokenize import TweetTokenizer
 
 
 class HelpMeTokenizer(TweetTokenizer):
-	
-	def tokenize(self, text):
+
+	def __init__(self):
+		# can try adjusting constructor if we're not getting decent results.
+		super(HelpMeTokenizer, self).__init__(preserve_case=False, reduce_len=True, strip_handles=True)
+
+	def process(self, text):
+		"""
+		Tokenize tweet, remove stopwords, 
+		twitter handles.
+		"""
+		tokenized = self.tokenize(text)
+		#print(tokenized)
 		
+
