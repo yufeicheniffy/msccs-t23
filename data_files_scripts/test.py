1b03fe745cb079c6ee3463d9f728d6d4ca58020d
Regan Dvoskin <rdvoskin@ccs.neu.edu>
2018-10-29 12:51:57 +0000

    fix path

--- data_files_scripts/test.py
+++ data_files_scripts/test.py
@@ -7,7 +7,7 @@ import json
 collection = MongoCollection()
 #database = DB("mongodb://Admin_1:glasgowcom@cluster0-shard-00-00-0yvu9.gcp.mongodb.net:27017,cluster0-shard-00-01-0yvu9.gcp.mongodb.net:27017,cluster0-shard-00-02-0yvu9.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", "mscdb", "msc")
 read = Reader("name of the reader")
-l = read.read("TRECIS-CTIT-H-Training.json")
+l = read.read("data_files/TRECIS-CTIT-H-Training.json")
 twitter = twitter_api("access_token", "access_token_secret", collection)
 #first parameter is the collection.
 collection.insert()
