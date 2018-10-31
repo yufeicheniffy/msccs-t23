879f8bc891613c6c9676fd6d6d968e0691efb8fc
2417455 <2417455D@student.gla.ac.uk>
2018-10-31 05:16:53 -0700

    frontend app now moved

--- frontend/.flaskenv
+++
@@ -1,1 +0,0 @@
-FLASK_APP=helpme.py
--- frontend/app/routes.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from flask import Flask, request, render_template
-
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
