fc88016eafea5e819c3f3295ad43cd18019a1b87
Panos <panosntoulos@gmail.com>
2018-11-29 23:55:21 +0000

    Update search_rest.py

--- app/search_rest.py
+++ app/search_rest.py
@@ -5,7 +5,12 @@ import pymongo
 import sys
 import json
 from bson import json_util, ObjectId
+from dateutil import parser
+import calendar
+
+
 #git example
+
 
 def query_search(query):
     access_token = "IHpSjYd5AuCdDRZTaGiMOwHUJ"
@@ -21,11 +26,16 @@ def query_search(query):
     id_list = []
     # You can change the cound the time limit of search.
     # moreover we can use Stream to be realy real_life project
-    for tweet in tweepy.Cursor(api.search, q=query, lang="en", count=10).items():
+    for tweet in tweepy.Cursor(api.search, q=query, lang="en", count=5).items():
         if(time.time()> time_started+2):
 			#mycol_all.insert(result_list)
+            print(type(datetime_created))
             return result_list, id_list
         # result_list.append(json.loads(json_util.dumps({"Postid": tweet["idstr"], "Text": tweet["text"]})))
-
-        result_list.append({"Postid": tweet._json["id_str"], "Text": tweet._json["text"]})
+        if ("media" in tweet._json["entities"]):
+            tweet_media = True
+        else:
+            tweet_media = False
+        datetime_created = parser.parse(tweet._json["created_at"])
+        result_list.append({"Postid": tweet._json["id_str"], "Text": tweet._json["text"], "Media": tweet_media, "Datetime": datetime_created, "DateString": tweet._json["created_at"], "timestamp": calendar.timegm(parser.parse(tweet._json["created_at"]).timetuple())})
         id_list.append(tweet._json["id_str"])
