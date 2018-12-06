addc896915e47ba47939d35a0618168e35141b45
Regan Dvoskin <rdvoskin@ccs.neu.edu>
2018-12-06 15:45:10 +0000

    begin testing classifier

--- test/classifier_tests.py
+++ test/classifier_tests.py
@@ -1,1 +1,105 @@
 import unittest
+import sys
+from sklearn.base import is_classifier
+import numpy as np
+
+sys.path.insert(0, '../..')
+sys.path.insert(0, '../classifier')
+
+from classifier.Classify_with_evaluation import Classify
+
+class TestClassifier(unittest.TestCase):
+
+    # test loading pretrained classifiers
+    # note: must actually have pretrained classifiers
+    # in classifier/pretrained
+    def test_load_pretrained(self):
+        c = Classify(pretrained = 'classifier/pretrained')
+
+        # check there classifiers loaded
+        self.assertIsNotNone(c.classifiers)
+        self.assertTrue(len(c.classifiers))
+        for classifier in c.classifiers:
+            self.assertTrue(is_classifier(classifier))
+
+        # check that vectorizer loaded
+        self.assertIsNotNone(c.vectorizer)
+
+    # check the simple evaluation method works as intended
+    def test_simple_eval(self):
+        base = np.array([[0, 1, 0, 0, 0],
+                         [0, 1, 0, 0, 1],
+                         [1, 0, 0, 1, 0],
+                         [1, 0, 0, 1, 0],
+                         [1, 1, 1, 1, 1]])
+        perf = base
+        close = np.array([[0, 1, 0, 1, 0],
+                          [0, 1, 0, 0, 1],
+                          [1, 0, 0, 1, 0],
+                          [1, 0, 0, 1, 0],
+                          [0, 0, 0, 0, 0]])
+        worst = np.array([[1, 0, 1, 1, 1],
+                          [1, 0, 1, 1, 0],
+                          [0, 1, 1, 0, 1],
+                          [0, 1, 1, 0, 1],
+                          [0, 0, 0, 0, 0]])
+        okay = np.array([[1, 1, 0, 0, 0],
+                         [0, 0, 0, 0, 1],
+                         [1, 0, 1, 1, 0],
+                         [1, 0, 0, 0, 0],
+                         [1, 1, 1, 1, 0]])
+        one_match_all = np.array([[0, 0, 1, 1, 1],
+                                  [1, 1, 1, 1, 0],
+                                  [0, 1, 0, 0, 1],
+                                  [0, 1, 1, 1, 1],
+                                  [0, 0, 0, 0, 1]])
+
+        c = Classify(pretrained = 'classifier/pretrained')
+
+        # check perfect prediction
+        t = c.simple_evaluation(base, perf)
+
+        keys = ['Number of Predictions',
+                'True Positive', 'True Negative',
+                'False Positive', 'False Negative',
+                'One Label', 'Perfect Match',
+                'One Label Score', 'Perfect Match Score',
+                'Accuracy', 'Precision', 'Recall',
+                'F1 Score']
+        self.assertTrue(all(key in t.keys() for key in keys))
+        self.assertTrue(all([
+            t['Number of Predictions'] == 25,
+            t['True Positive'] == 12,
+            t['True Negative'] == 13,
+            t['False Positive'] == 0,
+            t['False Negative'] == 0,
+            t['One Label'] == 5,
+            t['Perfect Match'] == 5,
+            t['One Label Score'] == 1,
+            t['Perfect Match Score'] == 1,
+            t['Accuracy'] == 1,
+            t['Precision'] == 1,
+            t['Recall'] == 1,
+            t['F1 Score'] == 1]))
+
+        cl = c.simple_evaluation(base, close)
+        self.assertTrue(all([
+            t['Number of Predictions'] == 25,
+            t['True Positive'] == 7,
+            t['True Negative'] == 12,
+            t['False Positive'] == 1,
+            t['False Negative'] == 5,
+            t['One Label'] == 4,
+            t['Perfect Match'] == 3,
+            t['One Label Score'] == 4/5,
+            t['Perfect Match Score'] == 3/5,
+            t['Accuracy'] == 19/25,
+            t['Precision'] == 7/8,
+            t['Recall'] == 7/12,
+            t['F1 Score'] == .7]))
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
