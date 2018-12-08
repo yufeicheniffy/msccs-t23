05db6b8739e5136cc5678b474941126c4f0bfa8f
Okereke Ngozi <ngzee@Macs-MacBook-Pro.local>
2018-12-08 14:42:25 +0000

    'undo my changes'

--- helpme.py
+++ helpme.py
@@ -1,7 +1,3 @@
 
 from app import app
 app.run(debug = True) # import package app and run the project
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
